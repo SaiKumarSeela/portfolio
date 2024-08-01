81a82776bd470dd431ea35069cd07e268f65797c
saikumar <sai680513@gmail.com>
2024-08-01 10:49:14 +0530

    add more skills

---
+++ js/skillset.js
@@ -0,0 +1,101 @@
+const skills = [{
+        title: "Web Development",
+        skills: [
+            { name: "HTML", image: "https://img.icons8.com/?size=48&id=v8RpPQUwv0N8&format=png" },
+            { name: "CSS", image: "https://img.icons8.com/?size=80&id=YjeKwnSQIBUq&format=png" },
+            { name: "JavaScript", image: "https://img.icons8.com/?size=48&id=PXTY4q2Sq2lG&format=png" },
+            { name: "Java", image: "https://img.icons8.com/?size=48&id=Pd2x9GWu9ovX&format=png" },
+            { name: "React", image: "https://img.icons8.com/?size=80&id=wPohyHO_qO1a&format=png" },
+            { name: "Flask", image: "https://img.icons8.com/?size=48&id=5mbMwDZ796xj&format=png" },
+            { name: "FastAPI", image: "https://tse1.mm.bing.net/th?id=OIP.BRGj6kGyDLxKr33lVKkUYgHaHa&pid=Api&rs=1&c=1&qlt=95&w=105&h=105" },
+            { name: "Django", image: "https://img.icons8.com/?size=80&id=37o3DqV429ra&format=png" }
+        ]
+    },
+    {
+        title: "Machine Learning",
+        skills: [
+            { name: "Python", image: "https://img.icons8.com/?size=80&id=hGdCwhSHUe6L&format=png" },
+            { name: "Scikitlearn", image: "https://tse1.mm.bing.net/th?id=OIP.CboBBbO8EdrFt8CUQ4EhiQHaE9&pid=Api&rs=1&c=1&qlt=95&w=159&h=106" },
+            { name: "TensorFlow", image: "https://tse1.mm.bing.net/th?id=OIP.zYVmDPXYGcNff5slxWWmRQHaH7&pid=Api&rs=1&c=1&qlt=95&w=103&h=110" },
+            { name: "PyTorch", image: "https://img.icons8.com/?size=64&id=O6SWwpPIM0GB&format=png" },
+            { name: "Pandas", image: "https://img.icons8.com/?size=48&id=xSkewUSqtErH&format=png" },
+            { name: "Numpy", image: "https://img.icons8.com/?size=48&id=aR9CXyMagKIS&format=png" },
+            { name: "Matplotlib", image: "https://tse4.mm.bing.net/th?id=OIP.AziU9GOm4K4iWoqIp4VQpQHaHa&pid=Api&P=0&h=180" },
+            { name: "Seaborn", image: "https://tse3.mm.bing.net/th?id=OIP.HXQ2EXkyOvy89obmJ_0kFgHaHa&pid=Api&P=0&h=180" },
+
+        ]
+    },
+    {
+        title: "Big Data",
+        skills: [
+            { name: "SQL", image: "https://img.icons8.com/?size=40&id=53562&format=png" },
+            { name: "MongoDB", image: "https://img.icons8.com/?size=80&id=8rKdRqZFLurS&format=png" },
+            { name: "PySpark", image: "https://tse2.mm.bing.net/th?id=OIP.Xk-Qzan0KWr4DJ9XwDRWmQHaDC&pid=Api&P=0&h=180" },
+            { name: "Kafka", image: "https://img.icons8.com/?size=64&id=fOhLNqGJsUbJ&format=png" },
+            { name: "Airflow", image: "https://tse2.mm.bing.net/th?id=OIP.Ukn5XLf8_MhdBzl8WHBSkAHaHa&pid=Api&P=0&h=180" },
+            { name: "Hadoop", image: "https://tse2.mm.bing.net/th?id=OIP.hbaCCYYHXnFAq3PY-Rg8vwHaFL&pid=Api&P=0&h=180" },
+
+        ]
+    },
+    {
+        title: "MLOps Tools",
+        skills: [
+            { name: "AWS Cloud", image: "https://img.icons8.com/?size=48&id=33039&format=png" },
+            { name: "Github actions", image: "https://img.icons8.com/?size=48&id=AZOZNnY73haj&format=png" },
+            { name: "Docker", image: "https://img.icons8.com/?size=48&id=cdYUlRaag9G9&format=png" },
+            { name: "Kubernetes", image: "https://img.icons8.com/?size=48&id=cvzmaEA4kC0o&format=png" },
+            { name: "MLflow", image: "https://tse1.mm.bing.net/th?id=OIP.OsB57V0FPteixDBD_BBN4gHaCt&pid=Api&P=0&h=180" },
+            { name: "DVC", image: "https://tse2.mm.bing.net/th?id=OIP.fvXDTOu9RB35vu28eWrPjAHaEP&pid=Api&P=0&h=180" },
+            { name: "BentoML", image: "https://tse2.mm.bing.net/th?id=OIP.ZQaod_MQyq5NeZ19lPbe3gAAAA&pid=Api&P=0&h=180" },
+            { name: "Linux", image: "https://img.icons8.com/?size=48&id=gE2kWztHPCpb&format=png" },
+            { name: "Grafana", image: "https://img.icons8.com/?size=48&id=bMkmDxPRZAld&format=png" },
+
+
+        ]
+    }, {
+        title: "Gen AI",
+        skills: [
+            { name: "LLMs", image: "https://tse3.mm.bing.net/th?id=OIP.f0F2bO6fR-flJgCmOGWXmAHaGL&pid=Api&P=0&h=180" },
+            { name: "LangChain", image: "https://tse3.mm.bing.net/th?id=OIP.IrUYFs0C1RQYIaXdT2TvYAHaFk&pid=Api&P=0&h=180" },
+            { name: "Nltk", image: "https://tse1.mm.bing.net/th?id=OIP.cMu8h5Pld-xu4K2W-cXQfgAAAA&pid=Api&P=0&h=180" },
+            { name: "APIs", image: "https://tse4.mm.bing.net/th?id=OIP.KRP5sv1xdY3RJnDge0jGswHaHa&pid=Api&P=0&h=180" },
+            { name: "Stable Diffusion", image: "https://tse2.mm.bing.net/th?id=OIP.UtCV2S4t8gvVbL7gD5wtVgAAAA&pid=Api&P=0&h=180" },
+            { name: "RAG Pipelines", image: "https://tse3.mm.bing.net/th?id=OIP.P9rwknh0dmAGD_G9P5MCCwHaHa&pid=Api&P=0&h=180" },
+            { name: "crewAI (AI agent)", image: "https://tse4.mm.bing.net/th?id=OIP.4LTbuktCcTYv7F_vuN-8sAHaCj&pid=Api&P=0&h=180" }
+
+
+        ]
+    }
+
+];
+
+const skillsListContainer = document.getElementById('skillsList');
+
+skills.forEach(skill => {
+    const skillCard = document.createElement('div');
+    skillCard.className = 'skill-card';
+
+    const skillTitle = document.createElement('h2');
+    skillTitle.className = 'skill-title';
+    skillTitle.textContent = skill.title;
+
+    const skillList = document.createElement('div');
+    skillList.className = 'skill-list';
+
+    skill.skills.forEach(item => {
+        const skillItem = document.createElement('div');
+        skillItem.className = 'skill-item';
+
+        const skillImage = document.createElement('img');
+        skillImage.src = item.image;
+        skillImage.alt = item.name;
+
+        skillItem.appendChild(skillImage);
+        skillItem.appendChild(document.createTextNode(item.name));
+        skillList.appendChild(skillItem);
+    });
+
+    skillCard.appendChild(skillTitle);
+    skillCard.appendChild(skillList);
+    skillsListContainer.appendChild(skillCard);
+});
